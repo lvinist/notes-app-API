922fa2e206147c3f01ec6f470fc5bf05da3ac270
Muhammad Alvin Ali Fikri <alvin.geomatics@gmail.com>
2021-06-11 16:19:28 +0700

    postgres database implemented

--- src/server.js
+++ src/server.js
@@ -1,17 +1,18 @@
 const Hapi = require('@hapi/hapi');
 const notes = require('./api/notes');
-const NotesService = require('./services/inMemory/notesService');
+const NotesService = require('./services/postgres/NotesService');
 const NotesValidator = require('./validation/notes');
 
 /* launch server on chrome using chrome.exe  --disable-site-isolation-trials
 --disable-web-security --user-data-dir="PATH_TO_PROJECT_DIRECTORY" */
+require('dotenv').config();
 
 const init = async () => {
   const notesService = new NotesService();
 
   const server = Hapi.server({
-    port: 5000,
-    host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
+    port: process.env.PORT,
+    host: process.env.HOST,
     routes: {
       cors: {
         origin: ['*'],
