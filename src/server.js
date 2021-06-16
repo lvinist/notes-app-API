2dbb1a7a758bbd05d5acb4eda5f8ba4a9d668c16
Muhammad Alvin Ali Fikri <alvin.geomatics@gmail.com>
2021-06-17 01:37:28 +0700

    user registration implemented

--- src/server.js
+++ src/server.js
@@ -2,6 +2,9 @@ const Hapi = require('@hapi/hapi');
 const notes = require('./api/notes');
 const NotesService = require('./services/postgres/NotesService');
 const NotesValidator = require('./validation/notes');
+const users = require('./api/users');
+const UsersService = require('./services/postgres/UsersService');
+const UsersValidator = require('./validation/users');
 
 /* launch server on chrome using chrome.exe  --disable-site-isolation-trials
 --disable-web-security --user-data-dir="PATH_TO_PROJECT_DIRECTORY" */
@@ -9,6 +12,7 @@ require('dotenv').config();
 
 const init = async () => {
   const notesService = new NotesService();
+  const usersService = new UsersService();
 
   const server = Hapi.server({
     port: process.env.PORT,
@@ -20,13 +24,22 @@ const init = async () => {
     },
   });
 
-  await server.register({
-    plugin: notes,
-    options: {
-      service: notesService,
-      validator: NotesValidator,
+  await server.register([
+    {
+      plugin: notes,
+      options: {
+        service: notesService,
+        validator: NotesValidator,
+      },
     },
-  });
+    {
+      plugin: users,
+      options: {
+        service: usersService,
+        validator: UsersValidator,
+      },
+    },
+  ]);
 
   await server.start();
   console.log(`Server berjalan pada ${server.info.uri}`);
